7148a07293f9115e35821876870a9c6d891d5eaa
nandorfn <firnandorfn@gmail.com>
2023-11-10 00:14:17 +0700

    update

--- src/components/JoinUs/index.jsx
+++ src/components/JoinUs/index.jsx
@@ -5,13 +5,11 @@ import "./JoinUs.css"
 export const JoinUs = () => {
     return (
       <div className="join">
-        <div className="row justify-content-center align-items-center" style={{ height: "100vh" }}>
+        <div className="row justify-content-center align-items-center" style={{ paddingBottom:'11rem', paddingTop: '13rem'}}>
             <div className="col-md-9">
                 <div className="h-100 p-5 text-center card-box">
-                    <h2 >Mari Bergabung Bersama Kami!</h2>
-                    <p>
-                    Semua orang bisa sehat. Penuhi kebutuhan sehatmu bersama kami. <br></br>
-                    Kami siap melayani anda
+                    <h2 className="fw-semibold">Mari Bergabung Bersama Kami!</h2>
+                    <p className="text-center mx-auto" style={{ maxWidth: '32rem'}}>Semua orang bisa sehat. Penuhi kebutuhan sehatmu bersama kami. Kami siap melayani anda
                     </p>
                     <Link to="https://play.google.com">
                         <img src={Play} alt="play" className="mt-2" style={{width:"200px"}} />
--- src/components/Pelayanan/index.jsx
+++ src/components/Pelayanan/index.jsx
@@ -1,60 +1,33 @@
 import Konsultasi from "../../image/Konsultasi Dokter.png"
 import Pembayaran from "../../image/Pembayaran 2.png"
 import Medicine from "../../image/Medicine.png"
-import Apotek from "../../image/Apotek.png"
+import Apotek from "../../image/apotek.png"
 import "./Pelayanan.css"
 
 export const Pelayanan = () => {
-    return (
-      <div className="mt-5 mb-5 d-flex justify-content-center align-items-center">
-        <div className="container p-0 m-5 m-md-4 row">
-            <div className="col-12 col-md-6 col-lg-3 kotak">
-                <div className="card shadow" style={{height:"412px"}}>
-                    <img src={Konsultasi} className="card-img-top" alt="pembayaran" style={{ height: "200px", objectFit: "cover" }}/>
-                    <div className="card-body">
-                        <h5 className="card-title text-center text-secondary">Konsultasi Semakin Mudah</h5>
-                        <p className="card-text text-center text-subline">
-                            Kami siap membantu kamu  untuk berkonsultasi dengan lebih mudah.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="col-12 col-md-6 col-lg-3 kotak">
-                <div className="card shadow" style={{height:"413px"}}>
-                    <img src={Pembayaran} className="card-img-top" alt="pembayaran" style={{ height: "200px", objectFit: "cover" }}/>
-                    <div className="card-body">
-                        <h5 className="card-title text-center text-secondary">Pembayaran Lebih Fleksibel</h5>
-                        <p className="card-text text-center text-subline">
-                            Kini tersedia berbagai metode pembayaran yang bisa kamu pilih.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="col-12 col-md-6 col-lg-3 kotak">
-                <div className="card shadow" style={{height:"414px"}}>
-                    <img src={Medicine} className="card-img-top" alt="medicine" style={{ height: "200px", objectFit: "cover" }}/>
-                    <div className="card-body">
-                        <h5 className="card-title text-center text-secondary">Produk Kesehatan Lengkap</h5>
-                        <p className="card-text text-center text-subline">
-                            Produk kesehatan tersedia secara lengkap. Temukan harga terjangkau untuk kamu.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="col-12 col-md-6 col-lg-3 kotak">
-                <div className="card shadow" style={{height:"414px"}}>
-                    <img src={Apotek} className="card-img-top" alt="apotek" style={{ height: "200px", objectFit: "cover" }}/>
-                    <div className="card-body">
-                        <h5 className="card-title text-center text-secondary">Pelayanan Apotek 24 Jam</h5>
-                        <p className="card-text text-center text-subline">
-                            Apotek 24 jam memudahkan kamu dalam membeli obat-obatan yang kamu butuhkan.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            
-        </div>
+    const listPelayanan = [
+        { img: Konsultasi, title: 'Konsultasi Semakin Mudah', content: 'Kami siap membantu kamu  untuk berkonsultasi dengan lebih mudah.' },
+        { img: Pembayaran, title: 'Pembayaran Lebih Fleksibel', content: 'Kini tersedia berbagai metode pembayaran yang bisa kamu pilih.' },
+        { img: Medicine, title: 'Produk Kesehatan Lengkap', content: 'Produk kesehatan tersedia secara lengkap. Temukan harga terjangkau untuk kamu.' },
+        { img: Apotek, title: 'Pelayanan Apotek 24 Jam', content: 'Apotek 24 jam memudahkan kamu dalam membeli obat-obatan yang kamu butuhkan.' },
+    ]
 
-      </div>
+    return (
+        <div className="mt-5 mb-5 d-flex justify-content-center align-items-center">
+            <div className="d-flex flex-column flex-md-row flex-wrap gap-3 align-items-center justify-content-center w-100 mx-auto">
+                {listPelayanan?.map((item, index) => (
+                    <div key={index} className="card rounded-4 border-0 shadow d-flex align-items-center justify-content-center" style={{ height: "412px", width: '16.87rem' }}>
+                        <img src={item.img} className="mt-4" alt="pembayaran" height={'190px'} />
+                        <div className="card-body">
+                            <h5 className="card-title text-center text-secondary fw-semibold">{item.title}</h5>
+                            <p className="card-text text-center text-subline">
+                                {item.content}
+                            </p>
+                        </div>
+                    </div>
+                ))
+                }
+            </div>  
+        </div>
     )
-  }
+}
