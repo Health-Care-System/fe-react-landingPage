8d5922dc8b27da4005b8fb24cd97273b511f335e
nandorfn <firnandorfn@gmail.com>
2023-11-24 12:45:18 +0700

    update: accordion

--- src/components/Acordion/index.jsx
+++ src/components/Acordion/index.jsx
@@ -1,11 +1,12 @@
+import './Accordion.css'
 export const Accordion = ({id, title, content}) => {
   return (
     <div 
       style={{maxWidth: '45rem', width: '100%'}}
-      className="accordion-item border-0 rounded-4">
+      className="accordion-item border-0 rounded-4 shadow-sm">
       <h2 className="accordion-header rounded-4">
         <button 
-          className="accordion-button bg-white border-0 shadow-sm rounded-4 text-primary fs-4 fw-bold" 
+          className="accordion-button bg-white rounded-4 text-primary fw-bold shadow-none title" 
           type="button" 
           data-bs-toggle="collapse" 
           data-bs-target={`#acordion${id}`} 
@@ -16,10 +17,10 @@ export const Accordion = ({id, title, content}) => {
       </h2>
       <div 
         id={`acordion${id}`} 
-        className="accordion-collapse collapse rounded-4">
+        className="accordion-collapse collapse">
         <div 
-          className="accordion-body text-green-300">
-          <p>{content}</p>
+          className="accordion-body pt-0 text-green-300">
+          <p className="border-2 border-top border-primary pt-3">{content}</p>
         </div>
       </div>
     </div>
